aab0c16a7730ae5497ea49e081715bab07696b37
Js-Yang <chyang590@gmaill.com>
2017-04-09 21:10:47 +0800

    extract production Specifications as dictionary

--- RefactoringKata/OrdersWriter.cs
+++ RefactoringKata/OrdersWriter.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace RefactoringKata
 {
@@ -43,25 +45,34 @@ namespace RefactoringKata
             for (var j = 0; j < order.GetProductsCount(); j++)
             {
                 var product = order.GetProduct(j);
+                Dictionary<string, object> productSpecifications = new Dictionary<string, object>()
+                {
+                    {"code",product.Code},
+                    {"color",getColorFor(product)},
+                    {"size",getSizeFor(product)},
+                    {"price",product.Price},
+                    {"currency",product.Currency}
+                };
+
                 sb.Append("{");
-                sb.Append("\"code\": \"");
-                sb.Append(product.Code);
+                sb.Append("\"" + productSpecifications.ElementAt(0).Key + "\": \"");
+                sb.Append(productSpecifications.ElementAt(0).Value);
                 sb.Append("\", ");
-                sb.Append("\"color\": \"");
-                sb.Append(getColorFor(product));
+                sb.Append("\"" + productSpecifications.ElementAt(1).Key + "\": \"");
+                sb.Append(productSpecifications.ElementAt(1).Value);
                 sb.Append("\", ");
                 if (product.Size != Product.SIZE_NOT_APPLICABLE)
                 {
-                    sb.Append("\"size\": \"");
-                    sb.Append(getSizeFor(product));
+                    sb.Append("\"" + productSpecifications.ElementAt(2).Key + "\": \"");
+                    sb.Append(productSpecifications.ElementAt(2).Value);
                     sb.Append("\", ");
                 }
 
-                sb.Append("\"price\": ");
-                sb.Append(product.Price);
+                sb.Append("\"" + productSpecifications.ElementAt(3).Key + "\": ");
+                sb.Append(productSpecifications.ElementAt(3).Value);
                 sb.Append(", ");
-                sb.Append("\"currency\": \"");
-                sb.Append(product.Currency);
+                sb.Append("\"" + productSpecifications.ElementAt(4).Key + "\": \"");
+                sb.Append(productSpecifications.ElementAt(4).Value);
                 sb.Append("\"}, ");
 
                 if (order.GetProductsCount() > 0)
